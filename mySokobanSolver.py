aaee9e01ce3cd912cf93bc9a34a58b0ba3035a54
Zaichic Turner <zaipowell@rocketmail.com>
2024-04-21 21:19:26 +1000

    Added solver part

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -163,42 +163,6 @@ def taboo_cells(warehouse):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-
-class SokobanPuzzle(search.Problem):
-    '''
-    An instance of the class 'SokobanPuzzle' represents a Sokoban puzzle.
-    An instance contains information about the walls, the targets, the boxes
-    and the worker.
-
-    Your implementation should be fully compatible with the search functions of 
-    the provided module 'search.py'. 
-    
-    '''
-    
-    #
-    #         "INSERT YOUR CODE HERE"
-    #
-    #     Revisit the sliding puzzle and the pancake puzzle for inspiration!
-    #
-    #     Note that you will need to add several functions to 
-    #     complete this class. For example, a 'result' method is needed
-    #     to satisfy the interface of 'search.Problem'.
-    #
-    #     You are allowed (and encouraged) to use auxiliary functions and classes
-
-    
-    def __init__(self, warehouse):
-        raise NotImplementedError()
-
-    def actions(self, state):
-        """
-        Return the list of actions that can be executed in the given state.
-        
-        """
-        raise NotImplementedError
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 def check_elem_action_seq(warehouse, action_seq):
     '''
     
@@ -298,6 +262,133 @@ def create_warehouse_representation(worker_x, worker_y, boxes, walls, targets):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+# Helper function for calculating distance:
+def distance(point1, point2):
+    return round(math.sqrt(pow(point2[0] - point1[0], 2) + pow(point2[1] - point1[1], 2)))
+
+# Helper function for determining if a given cell is taboo:
+def is_taboo(warehouse, coordinate):
+    lines = taboo_cells(warehouse).split('\n')
+    return lines[coordinate[1]][coordinate[0]] == 'X'
+
+class SokobanProblem(search.Problem):
+    def actions(self, state):
+        legal_actions = []
+
+        # Get the coordinates of the cells on each side of the worker
+        left_coord = (state.worker[0] - 1, state.worker[1])
+        right_coord = (state.worker[0] + 1, state.worker[1])
+        up_coord = (state.worker[0], state.worker[1] - 1)
+        down_coord = (state.worker[0], state.worker[1] + 1)
+
+        # Check there is no wall blocking left movement
+        if left_coord in state.walls:
+            legal_actions.append('left')
+        # Check there is no wall blocking right movement
+        if right_coord in state.walls:
+            legal_actions.append('right')
+        # Check there is no wall blocking up movement
+        if up_coord in state.walls:
+            legal_actions.append('up')
+        # Check there is no wall blocking down movement
+        if down_coord in state.walls:
+            legal_actions.append('down')
+        return legal_actions
+    
+    def result(self, state, action):
+        assert action in self.actions(state)
+        next_state = state.copy()
+        left_coord = (state.worker[0] - 1, state.worker[1])
+        right_coord = (state.worker[0] + 1, state.worker[1])
+        up_coord = (state.worker[0], state.worker[1] - 1)
+        down_coord = (state.worker[0], state.worker[1] + 1)
+        # Moving left
+        if action == 'left':
+            if left_coord in state.boxes:
+                index = state.boxes.index(left_coord)
+                next_state.boxes[index] = (left_coord[0] + 1, left_coord[1])
+            next_state.worker = (next_state.worker[0] - 1, next_state.worker[1])
+        # Moving right
+        if action == 'right':
+            if right_coord in state.boxes:
+                index = state.boxes.index(right_coord)
+                next_state.boxes[index] = (right_coord[0] + 1, right_coord[1])
+            next_state.worker = (next_state.worker[0] + 1, next_state.worker[1])
+        # Moving up
+        if action == 'up':
+            if up_coord in state.boxes:
+                index = state.boxes.index(up_coord)
+                next_state.boxes[index] = (up_coord[0] + 1, up_coord[1])
+            next_state.worker = (next_state.worker[0], next_state.worker[1] - 1)
+        # Moving down
+        if action == 'down':
+            if down_coord in state.boxes:
+                index = state.boxes.index(down_coord)
+                next_state.boxes[index] = (down_coord[0] + 1, down_coord[1])
+            next_state.worker = (next_state.worker[0], next_state.worker[1] + 1)
+        return next_state
+    
+    def h(self, node):
+        '''
+        Heuristic for goal state of the form range(k,-1,1) where k is a positive integer. 
+        h(n) = 1 + the sum of the weights of the boxes not at a target + an additional factor based on whether 
+        or not a box is in a taboo cell
+        '''
+        distance_factor = sum([distance(target, box) * weight for target, box, weight in zip(node.state.targets, node.state.boxes, node.state.weights)])
+        taboo_factor = float('Inf') if (len(list(filter(lambda x: is_taboo(node.state.warehouse, x), node.state.boxes))) != 0) else 0
+        return 1 + distance_factor + taboo_factor
+    
+    def print_solution(self, goal_node):
+        path = goal_node.path()
+        print("Solution takes {0} steps from the initial state\n".format(len(path) - 1))
+        print(path[0].state)
+        print("to the goal state\n")
+        print(path[-1].state)
+        print("Below is the sequence of moves\n")
+        for node in path:
+            self.print_node(node)
+
+    def print_node(self, node):
+        if node.action:
+            print("Move " + node.action)
+        print(node.state)
+
+class SokobanState:
+    def __init__(self, boxes, targets, walls, weights, worker, warehouse):
+        self.boxes = boxes
+        self.targets = targets
+        self.walls = walls
+        self.weights = weights
+        self.worker = worker
+        self.warehouse = warehouse
+    
+    def __str__(self):
+        X,Y = zip(*self.walls)
+        x_size, y_size = 1+max(X), 1+max(Y)
+        
+        vis = [[" "] * x_size for y in range(y_size)]
+        for (x,y) in self.walls:
+            vis[y][x] = "#"
+        for (x,y) in self.targets:
+            vis[y][x] = "."
+        if vis[self.worker[1]][self.worker[0]] == ".":
+            vis[self.worker[1]][self.worker[0]] = "!"
+        else:
+            vis[self.worker[1]][self.worker[0]] = "@"
+        for (x,y) in self.boxes:
+            if vis[y][x] == ".":
+                vis[y][x] = "*"
+            else:
+                vis[y][x] = "$"
+        return "\n".join(["".join(line) for line in vis])
+    
+    def __lt__(self, state):
+        return str(self) < str(state)
+
+    def copy(self):
+        clone = SokobanState(self.boxes, self.targets, self.walls, self.weights, self.worker, self.warehouse)
+        return clone
+
 def solve_weighted_sokoban(warehouse):
     '''
     This function analyses the given warehouse.
@@ -321,8 +412,21 @@ def solve_weighted_sokoban(warehouse):
             C is the total cost of the action sequence C
 
     '''
-    
-    raise NotImplementedError()
+    # Get Initial State:
+    initial_state = SokobanState(warehouse.boxes, warehouse.targets, warehouse.walls, warehouse.weights, warehouse.worker, warehouse)
+    # Get goal state:
+    goal_str = str(warehouse)
+    goal_str = goal_str.replace('$', ' ')
+    goal_str = goal_str.replace('.', '*')
+    goal_warehouse = sokoban.Warehouse()
+    goal_warehouse.from_string(goal_str)
+    goal_state = SokobanState(goal_warehouse.boxes, goal_warehouse.targets, goal_warehouse.walls, goal_warehouse.walls, goal_warehouse.worker, goal_warehouse)
+    # Create problem
+    problem = SokobanProblem(initial_state, goal=goal_state)
+    # Find solution node via A* graph search
+    solution_node = search.astar_graph_search(problem)
+    problem.print_solution(solution_node)
+    return ([], -1)
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
